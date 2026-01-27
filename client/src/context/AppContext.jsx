559cbfcf3d19e113d9721f2765e204a24bec9a66
Harsh Dhanawade <harshd2911@gmail.com>
2026-01-27 21:37:58 +0530

    Client-side UI and chat components implemented

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,6 +1,6 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-
+import { dummyUserData, dummyChats } from "../assets/assets"; // Add this import
 
 const AppContext = createContext()
 
@@ -10,7 +10,7 @@ export const AppContextProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [chats, setChats] = useState([]);
     const [selectedChat, setSelectedChat] = useState([]);
-    const [theme, steTheme] = useState(localStorage.getItem('theme' || 'light'));
+    const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light');
 
     const fetchUser = async () => {
         setUser(dummyUserData)
@@ -25,9 +25,8 @@ export const AppContextProvider = ({ children }) => {
         if (theme === 'dark') {
             document.documentElement.classList.add('dark')
         } else {
-        document.documentElement.classList.remove('dark')
-
-        }
+            document.documentElement.classList.remove('dark')
+        }localStorage.setItem('theme', theme)
     }, [theme])
     
 
@@ -46,7 +45,7 @@ export const AppContextProvider = ({ children }) => {
     },[])
 
     const value = {
-        navigate, user, setUser, fetchUser, chats, setChats, selectedChat, setSelectedChat, theme
+        navigate, user, setUser, fetchUser, chats, setChats, selectedChat, setSelectedChat, theme, setTheme
     }
     
     return (
